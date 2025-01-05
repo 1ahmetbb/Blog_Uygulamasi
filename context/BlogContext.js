4d308b78f58cfdc172f1703629a98ca1927a950d
Ahmet Baba <bbahmet13@gmail.com>
2025-01-05 16:08:53 +0300

    useReducer ile statik sekilde ekleme yapimi

--- context/BlogContext.js
+++ context/BlogContext.js
@@ -1,14 +1,24 @@
-import React, { useState } from "react";
+import React, { useState, useReducer } from "react";
 
 const BlogContext = React.createContext();
 
+const blogReducer = (state, action) => {
+  switch (action.type) {
+    case "add_blogpost":
+      return [...state, { title: "Vue Js" }];
+    default:
+      return state;
+  }
+};
+
 export const BlogProvider = ({ children }) => {
-  const [blogPost, setBlogPost] = useState([
+  const [blogPost, dispatch] = useReducer(blogReducer, [
     { title: "React Native" },
     { title: "Javascript" },
   ]);
+
   const addBlogPost = () => {
-    setBlogPost([...blogPost, { title: "Vue Js" }]);
+    dispatch({ type: "add_blogpost" });
   };
 
   return (
